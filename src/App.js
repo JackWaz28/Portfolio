f2267973f5a41ff89f3758ff6e6641258cb21b9c
JackWaz28 <jackw4@vt.edu>
2022-03-22 19:14:51 -0400

    Sidebar fixes and tailwind setup in index.css

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import logo from './logo.svg';
-import './App.css';
+import Sidebar from './Sidebar'
 
 function App() {
   return (
     <div className="App">
+    <Sidebar />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
